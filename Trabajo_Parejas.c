64f220431a4b22a3029729b4e58b0450ededd2eb
Dominic <dominic.cisneros@udla.edu.ec>
2026-04-11 18:55:51 -0500

    tercer commit

--- Trabajo_Parejas.c
+++ Trabajo_Parejas.c
@@ -6,11 +6,35 @@ int main(){
         for (int i = 0; i < 5 ; i++) {
             for (int j = 0; j < 3 ; j++) {
               printf("Escribe la nota: ");
-              scanf("%d", &notas[i][j]);  
+                scanf("%d", &notas[i][j]);  
+                 if (notas[i][j] < 0 || notas[i][j] > 10) {
+                    printf("Nota invalida\n");
+                    }            
             }
         }
+        for (int i = 0; i < 5; i++){
+            int suma = 0;
+            
+            for (int j = 0; i < 3 ; i++){
+                suma = suma + notas[i][j];
+            }
+            int promedio = suma /3;
+            printf("Estudiante %d: %d\n", i+1, promedio);           
+        }
+    printf("promedio por asignatura");
 
+        for (int j = 0; j < 3; j++){
+            int suma = 0;
+            
+            for (int  i = 0; i < 5; i++){
+                suma = suma + notas[i][j];
+            }
+            int promedio = suma / 5;
+            printf("Materia %d: %d\n", j+1, promedio);
 
+        }
+        
+        
 return 0;
 
 }
