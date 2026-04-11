992f2be577ba716f9d6ca07917af3126c2c4c483
Dominic <dominic.cisneros@udla.edu.ec>
2026-04-11 17:44:19 -0500

    segundo commit

--- Trabajo_Parejas.c
+++ Trabajo_Parejas.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 
+int main(){
+    
+     int notas[5][3];
+        for (int i = 0; i < 5 ; i++) {
+            for (int j = 0; j < 3 ; j++) {
+              printf("Escribe la nota: ");
+              scanf("%d", &notas[i][j]);  
+            }
+        }
+
+
+return 0;
+
+}
+    
